d88796f81d679eacb9756798f6a4d6f6911b6549
Fidel Thomet <me@fidelthomet.com>
2021-01-21 13:10:44 +0100

    update: drawing capabilities

--- src/components/ContributeCamera.vue
+++ src/components/ContributeCamera.vue
@@ -3,16 +3,29 @@
     <div v-show="!img1" class="video-wrapper">
       <video ref="video" playsinline autoplay :style="videoCover"/>
     </div>
-    <div v-if="img1" class="edit-wrapper">
+    <div v-show="img1" class="edit-wrapper">
       <img :src="img1"/>
+      <div class="drawing" ref="drawing"/>
     </div>
-    <base-button @click="capture">Take snapshot</base-button>
-
+    <div class="buttons">
+      <base-button v-if="!img1" @click="capture">Take Photo</base-button>
+      <base-button v-if="img1" @click="retake">Retake</base-button>
+      <base-button v-if="img1" @click="undo">Undo</base-button>
+    </div>
+    <div class="form">
+      <!-- <div class="label">Title</div> -->
+      <input type="text" v-model="title" placeholder="What did you capture?"/>
+      <!-- <div class="label">Description</div> -->
+      <textarea v-model="description" placeholder="What is the new normal?"/>
+      <base-button @click="submit" :disabled="!enableSubmit">Submit</base-button>
+      {{location.coords.latitude.toFixed(5) }} {{ location.coords.longitude.toFixed(5)}}
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import { SvgDrawing } from '@svg-drawing/core'
 // import { mapActions, mapGetters } from 'vuex'
 import BaseButton from './BaseButton.vue'
 export default {
@@ -24,7 +37,11 @@ export default {
       videoWidth: null,
       videoHeight: null,
       img1: null,
-      img2: null
+      img2: null,
+      drawing: null,
+      title: '',
+      description: '',
+      coords: null
     }
   },
   mounted () {
@@ -34,18 +51,16 @@ export default {
     this.$refs.video.srcObject = this.camera
   },
   computed: {
-    ...mapState('device', ['camera']),
+    ...mapState('device', ['camera', 'location']),
     videoCover () {
       return this.videoWidth > this.videoHeight ? { height: '100vw' } : { width: '100vw' }
+    },
+    enableSubmit () {
+      return this.captured && this.img1 && this.drawing && this.coords && this.title && this.description
     }
   },
   methods: {
     capture () {
-      if (this.captured) {
-        this.captured = false
-        this.img1 = null
-        return
-      }
       this.captured = true
       const video = this.$refs.video
       const canvas = document.createElement('canvas')
@@ -66,8 +81,34 @@ export default {
       }
       ctx.putImageData(imgData, 0, 0)
       this.img1 = canvas.toDataURL('image/jpeg', 0.7)
+      this.drawing = new SvgDrawing(this.$refs.drawing, { delay: 30 })
+
+      this.coords = [
+        +this.location.coords.latitude.toFixed(5),
+        +this.location.coords.longitude.toFixed(5)
+      ]
+    },
+    retake () {
+      this.captured = false
+      this.img1 = null
+      this.drawing = null
+      this.coords = null
+    },
+    undo () {
+      this.drawing.undo()
+    },
+    submit () {
+      console.log('submit')
     }
   }
+  // watch: {
+  //   'drawing.paths': {
+  //     handler (paths) {
+  //       console.log(paths)
+  //     },
+  //     deep: true
+  //   }
+  // }
 }
 </script>
 
@@ -75,7 +116,7 @@ export default {
 @import "@/assets/style/global";
 .contribute-camera {
   min-height: 100vh;
-  padding: $spacing * 8 $spacing $spacing $spacing;
+  padding: 0 $spacing $spacing $spacing;
   display: flex;
   flex-direction: column;
   background: $color-accent;
@@ -87,10 +128,25 @@ export default {
     align-items: center;
   }
 
-  .video-wrapper, img {
+  .video-wrapper, .edit-wrapper {
     margin: 0 #{-$spacing} #{$spacing};
     width: 100vw;
     height: 100vw;
+  }
+
+  img, .drawing {
+    width: 100vw;
+    height: 100vw;
+    position: absolute;
+  }
+
+  .drawing::v-deep {
+    svg {
+      path {
+        stroke: $color-accent;
+        stroke-width: 1.5vw;
+      }
+    }
   }
 
   video {
@@ -100,10 +156,51 @@ export default {
     filter: contrast(1.5) brightness(1.15);
   }
 
-  .base-button, .base-button-check {
-    margin-top: $spacing;
-    background: $color-white;
-    color: $color-accent;
+  .buttons {
+    display: flex;
+    .base-button {
+      // margin-top: $spacing;
+      background: $color-white;
+      color: $color-accent;
+
+      + .base-button {
+        margin-left: $spacing;
+      }
+    }
+  }
+
+  .form {
+    input, textarea {
+      display: block;
+      appearance: none;
+      border: none;
+      font-family: 'Firs';
+      font-size: 1em;
+      outline: none;
+      width: 100%;
+      padding: $spacing;
+      border-radius: $border-radius;
+      resize: none;
+      margin-top: $spacing;
+    }
+
+    textarea {
+      height: $spacing * 6;
+    }
+
+    .base-button {
+      margin-top: $spacing;
+      background: $color-white;
+      color: $color-accent;
+
+      &:disabled {
+        opacity: 0.2;
+      }
+
+      + .base-button {
+        margin-left: $spacing;
+      }
+    }
   }
 }
 </style>
